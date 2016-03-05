43a0bacbdc09d97b3abd9b54e660faaed8a9b358
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-03-04 17:33:39 -0800

    Change webpack config

--- webpack/config.js
+++ webpack/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'My TinyRx',
-  description: 'Manage your prescriptions with TinyRx. TinyRx brings you same-day, on-demand pharmacy delivery anywhere in the San Francisco and Los Angeles areas. Never wait in line for your pharmaceuticals again.',
+  title: 'M45H',
+  description: 'React Redux music player',
   rootHTML: '<main id="root"></main>',
   favicon: '<link rel="shortcut icon" type="image/ico" href="images/favicon.ico">'
 };
