b8861a54b2f04ffce87b87da7defbe2373e18490
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-03-04 17:54:20 -0800

    Update title

--- webpack/config.js
+++ webpack/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'M45H',
-  description: 'React Redux music player',
+  title: 'Redux Dashboard',
+  description: '',
   rootHTML: '<main id="root"></main>',
   favicon: '<link rel="shortcut icon" type="image/ico" href="images/favicon.ico">'
 };
